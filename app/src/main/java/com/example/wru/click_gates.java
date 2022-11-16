ee947fa02a669fe672b9a8f35fe8bfb7d36a9af8
LatrellAndrei <l-c1639@students.uc-bcf.edu.ph>
2022-11-16 17:51:48 +0800

    where are you java/xml

---
+++ app/src/main/java/com/example/wru/click_gates.java
@@ -0,0 +1,14 @@
+package com.example.wru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class click_gates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click_gates);
+    }
+}
